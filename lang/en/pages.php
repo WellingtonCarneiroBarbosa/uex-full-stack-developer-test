446641249bd1752e469bcc557f57a490caf1747a
wellington barbosa <wellingtonbarbosa.dev@gmail.com>
2024-06-17 05:22:13 -0300

    style: fix english translations

--- lang/en/pages.php
+++ lang/en/pages.php
@@ -3,21 +3,25 @@
 return [
     'auth' => [
         'login' => [
+            'title'           => 'Login',
             'remember-me'     => 'Remember me',
             'forgot-password' => 'Forgot your password?',
             'login'           => 'Log in',
             'have-no-account' => "Don't have an account yet?",
             'register-now'    => 'Register now!',
         ],
+
         'register' => [
+            'title'              => 'Create Account',
             'create-account'     => 'Create Account',
             'already-registered' => 'Already have an account?',
             'login-now'          => 'Login now!',
         ],
 
-
         'forgot-password' => [
+            'title'      => 'Forgot My Password',
             'disclaimer' => 'Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.',
+            'send'       => 'Send reset link',
         ],
     ],
 ];
